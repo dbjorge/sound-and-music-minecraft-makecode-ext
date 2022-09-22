f5834656f4e07665e15e2c51426d302180211d4f
Dan Bjorge <danielbj@microsoft.com>
2022-09-22 15:29:44 -0400

    implement initial support attempt at note editor

---
+++ src/instrument.ts
@@ -0,0 +1,76 @@
+/**
+ * Minecraft instrument types that a note block can emit
+ */
+declare const enum Instrument {
+    // default
+    Harp,
+
+    //% jres alias=PLANKS_OAK
+    Bass,
+    //% block="Snare Drum"
+    //% jres alias=SAND
+    SnareDrum,
+    // glass
+    //% block="Hi-hat"
+    HiHat,
+    // stone
+    //% block="Bass Drum"
+    BassDrum,
+    // gold block
+    Bell,
+    // clay
+    Flute,
+    // packed ice
+    Chime,
+    // wool
+    Guitar,
+    // bone block
+    Xylophone,
+    // iron block
+    Vibraphone,
+    // soul sand
+    //% block="Cow Bell"
+    CowBell,
+    // pumpkin
+    Didgeridoo,
+    // emerald block
+    Bit,
+    // hay bale
+    Banjo,
+    // glowstone
+    Pling,
+}
+
+namespace music {
+    /**
+     * Gets the Minecraft ID string representing an instrument.
+     * @param name the instrument name
+     */
+    //% weight=49
+    //% blockId=instrument_id block="%name"
+    //% shim=TD_ID color="#FFFFFF" colorSecondary="#FFFFFF"
+    //% name.fieldOptions.decompileLiterals=true
+    //% useEnumVal=1
+    //% group="Tone"
+    //% blockGap=8
+    export function instrumentId(name: Instrument): string {
+        switch (name) {
+            case Instrument.Bass: return 'note.bass';
+            case Instrument.SnareDrum: return 'note.snare';
+            case Instrument.HiHat: return 'note.hat';
+            case Instrument.BassDrum: return 'note.bd';
+            case Instrument.Bell: return 'note.bell';
+            case Instrument.Flute: return 'note.flute';
+            case Instrument.Chime: return 'note.chime';
+            case Instrument.Guitar: return 'note.guitar';
+            case Instrument.Xylophone: return 'note.xylophone';
+            case Instrument.Vibraphone: return 'note.iron_xylophone';
+            case Instrument.CowBell: return 'note.cow_bell';
+            case Instrument.Didgeridoo: return 'note.didgeridoo';
+            case Instrument.Bit: return 'note.bit';
+            case Instrument.Banjo: return 'note.banjo';
+            case Instrument.Pling: return 'note.pling';
+            case Instrument.Harp: return 'note.harp';
+        }
+    }
+}
--- src/note.ts
+++ src/note.ts
@@ -1,94 +1,94 @@
-declare const enum Note {
-    C = "0.707",
+// Taken from pxt-microbit /libs/core/music.ts,
+// but limited to Minecraft's note range (F#3 - F#5)
+//
+// This needs to map to frequency and not minecraft pitch
+// for compatibility with the "note" field editor
+enum Note {
+    //% blockIdentity=music.noteFrequency enumval=262
+    C = 262,
     //% block=C#
-    CSharp = "0.749",
-    D = "0.794",
-    Eb = "0.841",
-    E = "0.891",
-    F = "0.944",
+    //% blockIdentity=music.noteFrequency enumval=277
+    CSharp = 277,
+    //% blockIdentity=music.noteFrequency enumval=294
+    D = 294,
+    //% blockIdentity=music.noteFrequency enumval=311
+    Eb = 311,
+    //% blockIdentity=music.noteFrequency enumval=330
+    E = 330,
+    //% blockIdentity=music.noteFrequency enumval=349
+    F = 349,
     //% block=F#
-    FSharp = "1.000",
-    G = "1.059",
+    //% blockIdentity=music.noteFrequency enumval=370
+    FSharp = 370,
+    //% blockIdentity=music.noteFrequency enumval=392
+    G = 392,
     //% block=G#
-    GSharp = "1.122",
-    A = "1.189",
-    Bb = "1.260",
-    B = "1.335",
+    //% blockIdentity=music.noteFrequency enumval=415
+    GSharp = 415,
+    //% blockIdentity=music.noteFrequency enumval=440
+    A = 440,
+    //% blockIdentity=music.noteFrequency enumval=466
+    Bb = 466,
+    //% blockIdentity=music.noteFrequency enumval=494
+    B = 494,
 
     //% block=F#3
-    FSharp3 = "0.500",
-    G3 = "0.530",
+    //% blockIdentity=music.noteFrequency enumval=185
+    FSharp3 = 185,
+    //% blockIdentity=music.noteFrequency enumval=196
+    G3 = 196,
     //% block=G#3
-    GSharp3 = "0.561",
-    A3 = "0.595",
-    Bb3 = "0.630",
-    B3 = "0.667",
-    C4 = C,
+    //% blockIdentity=music.noteFrequency enumval=208
+    GSharp3 = 208,
+    //% blockIdentity=music.noteFrequency enumval=220
+    A3 = 220,
+    //% blockIdentity=music.noteFrequency enumval=233
+    Bb3 = 233,
+    //% blockIdentity=music.noteFrequency enumval=247
+    B3 = 247,
+    //% blockIdentity=music.noteFrequency enumval=262
+    C4 = 262,
     //% block=C#4
-    CSharp4 = CSharp,
-    D4 = D,
-    Eb4 = Eb,
-    E4 = E,
-    F4 = F,
+    //% blockIdentity=music.noteFrequency enumval=277
+    CSharp4 = 277,
+    //% blockIdentity=music.noteFrequency enumval=294
+    D4 = 294,
+    //% blockIdentity=music.noteFrequency enumval=311
+    Eb4 = 311,
+    //% blockIdentity=music.noteFrequency enumval=330
+    E4 = 330,
+    //% blockIdentity=music.noteFrequency enumval=349
+    F4 = 349,
     //% block=F#4
-    FSharp4 = FSharp,
-    G4 = G,
+    //% blockIdentity=music.noteFrequency enumval=370
+    FSharp4 = 370,
+    //% blockIdentity=music.noteFrequency enumval=392
+    G4 = 392,
     //% block=G#4
-    GSharp4 = GSharp,
-    A4 = A,
-    Bb4 = Bb,
-    B4 = B,
-    C5 = "1.414",
+    //% blockIdentity=music.noteFrequency enumval=415
+    GSharp4 = 415,
+    //% blockIdentity=music.noteFrequency enumval=440
+    A4 = 440,
+    //% blockIdentity=music.noteFrequency enumval=466
+    Bb4 = 466,
+    //% blockIdentity=music.noteFrequency enumval=494
+    B4 = 494,
+    //% blockIdentity=music.noteFrequency enumval=523
+    C5 = 523,
     //% block=C#5
-    CSharp5 = "1.498",
-    D5 = "1.587",
-    Eb5 = "1.682",
-    E5 = "1.782",
-    F5 = "1.888",
+    //% blockIdentity=music.noteFrequency enumval=555
+    CSharp5 = 555,
+    //% blockIdentity=music.noteFrequency enumval=587
+    D5 = 587,
+    //% blockIdentity=music.noteFrequency enumval=622
+    Eb5 = 622,
+    //% blockIdentity=music.noteFrequency enumval=659
+    E5 = 659,
+    //% blockIdentity=music.noteFrequency enumval=698
+    F5 = 698,
     //% block=F#5
-    FSharp5 = "2.000",
-}
-
-/**
- * Minecraft instrument types that a note block can emit
- */
-declare const enum Instrument {
-    //% jres alias=PLANKS_OAK
-    Bass = "note.bass",
-    //% block="Snare Drum"
-    //% jres alias=SAND
-    SnareDrum = "note.snare",
-    // glass
-    //% block="Hi-hat"
-    HiHat = "note.hat",
-    // stone
-    //% block="Bass Drum"
-    BassDrum = "note.bd",
-    // gold block
-    Bell = "note.bell",
-    // clay
-    Flute = "note.flute",
-    // packed ice
-    Chime = "note.chime",
-    // wool
-    Guitar = "note.guitar",
-    // bone block
-    Xylophone = "note.xylophone",
-    // iron block
-    Vibraphone = "note.iron_xylophone",
-    // soul sand
-    //% block="Cow Bell"
-    CowBell = "note.cow_bell",
-    // pumpkin
-    Didgeridoo = "note.didgeridoo",
-    // emerald block
-    Bit = "note.bit",
-    // hay bale
-    Banjo = "note.banjo",
-    // glowstone
-    Pling = "note.pling",
-    // default
-    Harp = "note.harp",   
+    //% blockIdentity=music.noteFrequency enumval=740
+    FSharp5 = 740,
 }
 
 namespace music {
@@ -96,35 +96,45 @@ namespace music {
      * Plays a note similar to a Minecraft note block.
      * @param note pitch of the tone to play in Hertz (Hz), eg: Note.C
      * @param instrument instrument to play the note as
-     * 
-     * @param ms tone duration in milliseconds (ms)
      */
-    //% help=music/play-note weight=90
+    //% weight=90
     //% blockId=music_play_note block="play|note $note=note_pitch|on $instrument" blockGap=8
     //% parts="headphone"
     //% useEnumVal=1
     //% group="Notes"
-    //% note.defl="1.414"
+    //% note.defl="262"
     //% instrument.defl=Instrument.Harp
     export function playNote(note: Note, instrument: Instrument): void {
-        const soundId = instrument.toString();
-        const pitch = note.toString();
+        const soundId: string = instrumentId(instrument);
+        const pitch: string = frequencyToMinecraftPitch(note);
         player.execute(`playsound ${soundId} @a ~ ~ ~ ${music.volumeInGameUnits} ${pitch}`)
     }
 
     /**
-     * Gets the pitch of a note.
+     * Gets the frequency of a note in Hertz.
      * @param name the note name
      */
-    //% weight=50 help=music/note-pitch
-    //% blockId=note_pitch block="%name"
+    //% weight=50 help=music/note-frequency
+    //% blockId=note_frequency block="%name"
     //% shim=TD_ID color="#FFFFFF" colorSecondary="#FFFFFF"
-    //% name.fieldEditor="note" name.defl="1.414"
+    //% name.fieldEditor="note" name.defl="262"
     //% name.fieldOptions.decompileLiterals=true
     //% useEnumVal=1
-    //% group="Tone"
+    //% group="Notes"
     //% blockGap=8
-    export function notePitch(name: Note): string {
+    export function noteFrequency(name: Note): number {
         return name;
+    }
+
+    export function frequencyToMinecraftPitch(frequencyHz: number): string {
+        // The minecraft note scale runs from F#3 (pitch .5) to F#5 (pitch 2.0).
+        const pitch = Math.map(frequencyHz, 185, 740, .5, 2.0);
+        return formatFloatForMinecraftCommand(pitch);
+    }
+
+    // This is just number.toFixed(3)
+    function formatFloatForMinecraftCommand(input: number): string {
+        const thousandths = Math.round(input * 1000);
+        return `${Math.floor(thousandths / 1000)}.${thousandths % 1000}`;
     }
 }
